06bd9013868e96f9d1c6330ed958bd90c3400201
hritvikkaushik <hritvikkaushik@gmail.com>
2021-02-05 21:37:24 +0530

    Made TopBar into class based component

--- src/containers/TopBar/TopBar.js
+++ src/containers/TopBar/TopBar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './TopBar.css';
 import axios from 'axios';
@@ -8,54 +8,71 @@ const key = 'pk.eyJ1IjoiaHJpdHZpa2thdXNoaWsiLCJhIjoiY2trcjZhb2ZoMDQ5bzJ3b2kya2U5
 const base = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
 const call = (q) => `${base}${q}.json?access_token=${key}&autocomplete=true`;
 
-const topBar = () => {
+class topBar extends Component {
 
-    const [suggestions, setSuggestions] = useState([]);
-    const [boxVisible, setBoxVisible] = useState(false);
+    state = {
+        suggestions: [],
+        boxVisible: false,
+    }
 
-    const searchingHandler = (event) => {
+    searchingHandler = (event) => {
         const s = event.target.value;
 
         if(s.length>=3){
             console.log(call(s));
             axios.get(call(s)).then( response => {
                 console.log(response.data.features[2].place_name);
-                setSuggestions(response.data.features.map(feature=>{
-                    return{
-                        id: feature.id,
-                        place_name: feature.place_name,
-                        text: feature.text,
-                        coordinates: feature.geometry.coordinates
-                    }
-                }));
-                setBoxVisible(true);
+                this.setState({
+                    suggestions: response.data.features.map(feature=>{
+                        return{
+                            id: feature.id,
+                            place_name: feature.place_name,
+                            text: feature.text,
+                            coordinates: feature.geometry.coordinates
+                        }
+                    }),
+                    boxVisible: true,
+                });
             });
         }
         
         if(!s) {
-            setSuggestions([]);
-            setBoxVisible(false);
+            this.setState({
+                suggestions: [],
+                boxVisible: false,
+            });
         }
     }
 
-    const clickHandler = (place) => {
+    clickHandler = (place) => {
         console.log(place.place_name);
         console.log(place.id);
         console.log(place.text);
     }
 
-    return(
-        <Aux>
-            <div className={classes.Bar}>
-                <input className={classes.Box} onChange={searchingHandler} type="text" placeholder="Search for city..."/>
-            </div>
-            <div className={boxVisible?classes.SearchSuggestions:null}>
-                {suggestions.map(s => <div id={s.id} 
-                                        className={classes.suggestion}
-                                        onClick={() => clickHandler(s)}><p>{s.place_name}</p></div>)}
-            </div>
-        </Aux>
-    )
+    render() {
+
+        let searchSuggestions = null;
+
+        if(this.state.boxVisible){
+            searchSuggestions = this.state.suggestions.map(s => <div id={s.id} 
+                                            className={classes.suggestion}
+                                            onClick={() => this.clickHandler(s)}>
+                                                <p>{s.place_name}</p>
+                                            </div>)
+        } else searchSuggestions = null;
+
+        return(
+            <Aux>
+                <div className={classes.Bar}>
+                    <input className={classes.Box} onChange={this.searchingHandler} type="text" placeholder="Search for city..."/>
+                </div>
+                <div className={this.state.boxVisible?classes.SearchSuggestions:null}>
+                    {searchSuggestions}
+                </div>
+            </Aux>
+        )
+    }
 }
 
 export default topBar;
